b4499f20eba1506be88eaf222b9102a2893480e3
“mosesglitch” <mosesgachuru15@gmail.com>
2024-01-30 12:44:17 +0300

    add library addon

---
+++ library/library_app/__manifest__.py
@@ -0,0 +1,17 @@
+{
+"name": "Library Management","summary": "Manage library catalog and book lending.",
+"author": "Daniel Reis",
+"license": "AGPL-3",
+"website": "https://github.com/PacktPublishing"
+"/Odoo-15-Development-Essentials",
+# to getmore information about the module.
+"category": "Services/Library",
+"version": "15.0.1.0.0",
+"data": [
+"security/library_security.xml",
+"security/ir.model.access.csv",
+"views/library_menu.xml",
+],
+"depends": ["base"],
+"application": True,
+}
